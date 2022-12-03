c4b84503fa3ef58a291007f46d7f47d8695687f0
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-12-03 12:19:32 -0700

    Adds more styling to movieTiles.css

--- src/MovieTile/MovieTile.js
+++ src/MovieTile/MovieTile.js
@@ -4,7 +4,7 @@ import './MovieTile.css'
 const MovieTile = ({id , poster, showMore}) => {
   return (
     <div className='movie-tile' >
-     <button onClick={() => showMore(id)}><img className='tile-img' src={poster} ></img></button>
+     <button className='tile-button' onClick={() => showMore(id)}><img className='tile-img' src={poster} ></img></button>
     </div>
   )
 }
