5a42536ddbbd7dc589f3341fd8aa8826b58d3f7d
Matt Walter <matt.walter.tx@outlook.com>
2022-12-12 17:39:10 -0600

    Fix: correct render and page items of 404

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
-// import '/NotFound.css'
+import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='not-found'>
       <h1>404 - Not Found!</h1>
-      <Link to="/">Go Home</Link>
+      <h1>Please navigate back to the home page with your browser or the link below</h1>
+      <Link to="/"><h2>RETURN HOME</h2></Link>
     </div>
   )
 }
