700cc5cca70355a012a989577f9eb5e204e7c5c2
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-12-12 16:57:21 -0700

    Adds a button to error page

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -2,12 +2,12 @@ import React from "react"
 import { Link } from "react-router-dom"
 import './NotFound.css'
 
-const NotFound = () => {
+const NotFound = ({showAll}) => {
   return (
     <div className='not-found'>
       <h1>404 - Not Found!</h1>
       <h1>Please navigate back to the home page with your browser or the link below</h1>
-      <Link to="/"><h2>RETURN HOME</h2></Link>
+      <Link to="/"><h2><button className='error-return-button' onClick={showAll}>RETURN HOME</button></h2></Link>
     </div>
   )
 }
