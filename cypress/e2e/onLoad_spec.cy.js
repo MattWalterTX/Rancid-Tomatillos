c3e6b3596ad1a6e2825d69a99283c3adc97879eb
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-12-12 13:36:26 -0700

    Refactors the testing for the first userflow

--- cypress/e2e/onLoad_spec.cy.js
+++ cypress/e2e/onLoad_spec.cy.js
@@ -4,8 +4,7 @@
   });
 
   it('Should see the title of the application', () => {
-      cy.get('p')
-        .contains('Rotten Tomatillos')
+      cy.get('.site-title')
   });
 
   it('Should see a collection of movies', () => {
@@ -13,18 +12,18 @@
   });
 
   it('Should be able to click a movie and see more info', () => {
-    cy.get('#694919').click()
+    cy.visit('http://localhost:3000/436270')
       .get('.single-view')
   });
 
   it('Should be able to see more info', () => {
-    cy.get('#694919').click()
+    cy.get('#436270').click()
     cy.get('.single-view')
       .get('.info')
   });
 
   it('Should be able to click a button and go home', () => {
-    cy.get('#694919').click()
+    cy.get('#436270').click()
     cy.get('.single-view')
       .get('.info')
       .get('.return-button').click()
