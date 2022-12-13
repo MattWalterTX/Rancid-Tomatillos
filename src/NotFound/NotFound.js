8363f076eba2b4adf5b521a7162830905e804429
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-12-12 17:04:39 -0700

    Adds proptypes to NotFound

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom"
 import './NotFound.css'
 
@@ -13,3 +14,6 @@ const NotFound = ({showAll}) => {
 }
 
 export default NotFound
+NotFound.propTypes = {
+  showAll: PropTypes.func,
+}
